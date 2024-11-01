62447da90182b7db2d1a31809cf26585cd3c6b07
Angeline B <bedisad@students.nu-moa.edu.ph>
2024-11-01 17:38:35 +0800

    custom design for protected route

--- website-admin/src/components/ProtectedRoute.js
+++ website-admin/src/components/ProtectedRoute.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
+import { Button, Result } from 'antd';
+import '../styles/ProtectedRoute.css';
+import logo1 from '../assets/Logo 1.png'; 
+import logo2 from '../assets/Logo 2.png'; 
+import '../styles/Login.css'; 
 
 const ProtectedRoute = ({ children }) => {
   const isAuthenticated = localStorage.getItem("isAuthenticated");
-    // If the user is not logged in then they cannot access the site via url
-  return isAuthenticated ? children : <Navigate to="/" replace />;
+  const location = useLocation();
+
+  // If the user is not logged in, show the unauthorized message
+  if (!isAuthenticated) {
+    return (
+      <div className="protected-route">
+        <div className='login-divider'>
+          <img src={logo1} className="login-image" alt="Logo 1" />
+          <div className="divider" />
+          <img src={logo2} className="login-image" alt="Logo 2" />
+        </div>
+        <Result
+          status="403"
+          title="403"
+          subTitle="Sorry, you are not authorized to access this page."
+          extra={
+            <Link to="/" state={{ from: location }}>
+              <Button type="primary">Go to Login</Button>
+            </Link>
+          }
+        />
+      </div>
+    );
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
